f16ce1bc8340a7cb1eb883681dd563192858a2f2
soobin <mhoang26ct@gmail.com>
2026-05-04 22:05:55 +0700

    refactor(cinema): migrate cinema list endpoint to public controller and add MovieFormat enum mapping to price configuration

---
+++ src/main/java/com/uit/backend_cinema/modules/cinema/api/controller/OpenCinemaController.java
@@ -0,0 +1,33 @@
+package com.uit.backend_cinema.modules.cinema.api.controller;
+
+import com.uit.backend_cinema.common.util.ApiResponse;
+import com.uit.backend_cinema.modules.cinema.api.entity.CinemaResponseDTO;
+import com.uit.backend_cinema.modules.cinema.api.mapper.CinemaApiMapper;
+import com.uit.backend_cinema.modules.cinema.domain.service.CinemaService;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/v1/public/cinemas")
+public class OpenCinemaController {
+
+    private final CinemaService cinemaService;
+    private final CinemaApiMapper mapper;
+
+    public OpenCinemaController(CinemaService cinemaService, CinemaApiMapper mapper) {
+        this.cinemaService = cinemaService;
+        this.mapper = mapper;
+    }
+
+    @GetMapping
+    public ResponseEntity<?> getAllCinemas() {
+        List<CinemaResponseDTO> response = cinemaService.findAll().stream()
+                .map(mapper::toDTO)
+                .toList();
+        return ResponseEntity.ok(ApiResponse.success(response, "Lấy danh sách rạp thành công"));
+    }
+}
