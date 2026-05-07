015d6f7edcea511c2a7600361e7e2b2f37cccf7f
soobin <mhoang26ct@gmail.com>
2026-05-07 16:30:31 +0700

    Chore(Food): adding mapping ignorant rules to clarify

--- src/main/java/com/uit/backend_cinema/modules/food_order/api/mapper/FoodOrderItemApiMapper.java
+++ src/main/java/com/uit/backend_cinema/modules/food_order/api/mapper/FoodOrderItemApiMapper.java
@@ -11,5 +11,7 @@ public interface FoodOrderItemApiMapper {
     @Mapping(target = "total", expression = "java(foodOrderItem.getPrice().multiply(new java.math.BigDecimal(foodOrderItem.getQuantity())))")
     FoodOrderItemResponseDTO toResponseDTO(FoodOrderItem foodOrderItem);
 
+    @Mapping(target = "foodName", ignore = true)
+    @Mapping(target = "price", ignore = true)
     FoodOrderItem toDomain(FoodOrderItemRequestDTO foodOrderItemDTO);
 }
